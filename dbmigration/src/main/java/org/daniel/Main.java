4e81623f21ba808cd5ecae98858dce321b67dee9
daniel alzueta <daniel.alzueta@astropay.com>
2024-09-10 01:21:07 -0300

    update:

--- dbmigration/src/main/java/org/daniel/Main.java
+++ dbmigration/src/main/java/org/daniel/Main.java
@@ -1,8 +1,39 @@
 package org.daniel;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Initializing db migration!");
+
+    // Configuración de la conexión a la base de datos
+    String url = "jdbc:mariadb://localhost:3306/authdb";
+    String username = "root";
+    String password = "example";
+
+    try {
+      Connection connection = DriverManager.getConnection(url, username, password);
+      Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+      Liquibase liquibase = new Liquibase("src/main/resources/db/migration/changelog-master.xml", new ClassLoaderResourceAccessor(), database);
+
+      liquibase.update(new Contexts());
+      System.out.println("Database migration completed successfully!");
+
+    } catch (SQLException | LiquibaseException e) {
+      e.printStackTrace();
+      System.err.println("Error during database migration: " + e.getMessage());
+    }
   }
 }
